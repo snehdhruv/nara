cf64594844f8563ddaf703430386985a3f3416d4
Vedant Singh <vedant.singh01@sjsu.edu>
2025-08-22 22:52:50 -0700

    switch to desktop

---
+++ src/components/convex-client-provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ConvexReactClient } from "convex/react";
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import { authClient } from "@/lib/auth-client";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
+
+export function ConvexClientProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+      {children}
+    </ConvexBetterAuthProvider>
+  );
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,47 @@
+import { convexAdapter } from "@convex-dev/better-auth";
+import { convex, crossDomain } from "@convex-dev/better-auth/plugins";
+import { requireEnv } from "@convex-dev/better-auth/utils";
+import { betterAuth } from "better-auth";
+import { betterAuthComponent } from "../../convex/auth.js";
+import { type GenericCtx } from "../../convex/_generated/server";
+
+const siteUrl = requireEnv("SITE_URL");
+
+export const createAuth = (ctx: GenericCtx) =>
+  // Configure your Better Auth instance here
+  betterAuth({
+    trustedOrigins: [siteUrl],
+    database: convexAdapter(ctx, betterAuthComponent),
+
+    // Simple non-verified email/password to get started
+    emailAndPassword: {
+      enabled: true,
+      requireEmailVerification: false,
+    },
+
+    socialProviders: {
+        spotify: {
+            clientId: process.env.AUTH_SPOTIFY_ID as string, 
+            clientSecret: process.env.AUTH_SPOTIFY_SECRET as string,
+            scope: [
+                "user-read-email",
+                "user-read-private", 
+                "user-read-currently-playing",
+                "user-read-playback-state",
+                "user-read-recently-played",
+                "user-modify-playback-state",
+                "streaming"
+            ]
+        }
+    },
+
+    plugins: [
+      // The Convex plugin is required
+      convex(),
+
+      // The cross domain plugin is required for client side frameworks
+      crossDomain({
+        siteUrl,
+      }),
+    ],
+  });
